b396a69b41fdd24ef2940e68c966e5b01ed9540f
Julien Sulpis <julien.sulpis@gmail.com>
2021-02-18 20:46:26 +0100

    feat: add introduction scene

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -7,56 +7,55 @@
 <script setup>
 import { onMounted } from "vue";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 onMounted(() => {
-  /**
-   * Base
-   */
   const canvas = document.querySelector("canvas.webgl");
   const scene = new THREE.Scene();
+  const ambientLight = new THREE.AmbientLight("#fff", 8);
+  scene.add(ambientLight);
 
-  /**
-   * Object
-   */
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load("/intro.glb", (glb) => {
+    scene.add(glb.scene);
+  });
 
-  /**
-   * Sizes
-   */
   const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
   };
 
-  /**
-   * Camera
-   */
-  // Base camera
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-  );
-  camera.position.z = 3;
+  const camera = new THREE.PerspectiveCamera(33, sizes.width / sizes.height);
+  camera.position.set(0, 1.2, 8);
   scene.add(camera);
 
-  // Controls
   const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
+  controls.target = new THREE.Vector3(0, 1.4, 0);
 
-  /**
-   * Renderer
-   */
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
+    antialias: true,
   });
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setClearColor(0x333333);
+  renderer.physicallyCorrectLights = true;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+
+  window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 
   const tick = () => {
     controls.update();
