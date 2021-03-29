0cf221f5b10459b197cf0fc3eebcc4a14ba2f6a4
Julien Sulpis <julien.sulpis@gmail.com>
2021-03-29 22:19:23 +0200

    feat: finish the perf page

--- src/components/sections/08-three-perf/ThreePerf.vue
+++ src/components/sections/08-three-perf/ThreePerf.vue
@@ -10,16 +10,36 @@
 import { onMounted } from "vue";
 import * as THREE from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { SkeletonUtils } from "three/examples/jsm/utils/SkeletonUtils";
 import useThreeBasicScene from "../../../hooks/useThreeBasicScene";
+import Stats from "stats.js";
 
 onMounted(() => {
+  // Stats
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  const statsFps = new Stats();
+  statsFps.showPanel(0);
+  statsContainer.appendChild(statsFps.dom);
+
+  const statsMs = new Stats();
+  statsMs.showPanel(1);
+  statsContainer.appendChild(statsMs.dom);
+
+  const statsMemory = new Stats();
+  statsMemory.showPanel(2);
+  statsContainer.appendChild(statsMemory.dom);
+
+  document.getElementById("three-perf").appendChild(statsContainer);
+
+  // Base
   const canvas = document.querySelector<HTMLCanvasElement>("canvas#threePerf");
   const { scene, camera, renderer, controls } = useThreeBasicScene(canvas, 0);
 
-  // General adjustments
   camera.setFocalLength(30);
-  camera.position.set(0, 150, 700);
-  controls.target.set(0, 50, 0);
+  camera.position.set(500, 100, 500);
+  controls.target.set(750, 80, 750);
   controls.update();
   renderer.setClearAlpha(0);
 
@@ -30,12 +50,6 @@ onMounted(() => {
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
   directionalLight.position.set(-100, 400, -40);
-  directionalLight.castShadow = true;
-  directionalLight.shadow.camera.top = 100;
-  directionalLight.shadow.camera.bottom = -100;
-  directionalLight.shadow.camera.left = -100;
-  directionalLight.shadow.camera.right = 100;
-  directionalLight.shadow.radius = 20;
   scene.add(directionalLight);
 
   // Ground
@@ -48,33 +62,67 @@ onMounted(() => {
   scene.add(ground);
 
   // Model
-  const fbxLoader = new FBXLoader();
-  let mixer: THREE.AnimationMixer, jumpAction: THREE.AnimationAction;
-  fbxLoader.load("/models/zenikanard_LOD1.fbx", (model) => {
-    model.scale.set(10, 10, 10);
-    model.rotation.y = (3 * Math.PI) / 4;
-    model.children[0].castShadow = true;
+  let mixer: THREE.AnimationMixer;
 
+  new FBXLoader().load("/models/zenikanard_LOD1.fbx", (model) => {
+    model.scale.set(10, 10, 10);
+    model.position.set(750, 0, 750);
     mixer = new THREE.AnimationMixer(model);
 
-    jumpAction = mixer.clipAction(model.animations[0]);
-    jumpAction.play();
+    mixer.clipAction(model.animations[0]).play();
     scene.add(model);
+
+    const SQUARE_SIZE = 500;
+    for (let i = 0; i < 100; i++) {
+      const modelCopy = SkeletonUtils.clone(model);
+      modelCopy.rotation.y = Math.random() * 2 * Math.PI;
+      modelCopy.position.x = -SQUARE_SIZE + Math.random() * 2 * SQUARE_SIZE;
+      modelCopy.position.z = -SQUARE_SIZE + Math.random() * 2 * SQUARE_SIZE;
+
+      mixer
+        .clipAction(model.animations[0], modelCopy)
+        .startAt(i / 10)
+        .play();
+
+      scene.add(modelCopy);
+    }
   });
 
   // Animation
   const clock = new THREE.Clock();
   (function tick() {
+    statsMs.begin();
+    statsFps.begin();
+    statsMemory.begin();
+
     mixer && mixer.update(clock.getDelta());
     renderer.render(scene, camera);
 
+    statsMs.end();
+    statsFps.end();
+    statsMemory.end();
     requestAnimationFrame(tick);
   })();
 });
 </script>
 
-<style scoped>
+<style lang="scss">
 #three-perf {
   background: linear-gradient(#25446b, #0c1a2a);
+}
+
+.stats-container {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  > div {
+    position: initial !important;
+    transform: scale(1.5);
+    transform-origin: top left;
+  }
 }
 </style>
